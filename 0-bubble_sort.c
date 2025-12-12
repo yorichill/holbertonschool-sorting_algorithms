43212c5e0953bad8bb19051d7a6b6ced8d75dbd9
yorichill <thomas.haenel101@gmail.com>
2025-12-12 12:43:24 +0100

    new version

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,37 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- * @array: The array to be sorted
- * @size: Number of elements in @array
+ * bubble_sort - Trie un tableau d'entiers par ordre croissant
+ * @array: Le tableau à trier
+ * @size: La taille du tableau
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
+	int swapped;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
+				/* Échange (Swap) */
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				/* Appel obligatoire après chaque swap */
+				swapped = 1;
+				/* Affiche le tableau après chaque échange */
 				print_array(array, size);
 			}
 		}
+		/* Si aucun échange n'a eu lieu, le tableau est déjà trié */
+		if (swapped == 0)
+			break;
 	}
 }
--- sort.h
+++ sort.h
@@ -1,13 +1,11 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
- *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -19,14 +17,13 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-/* Helper functions (Fournies dans votre projet) */
+/* Prototypes fournis par Holberton pour l'affichage */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Prototypes des algorithmes de tri */
+/* Prototypes des algorithmes */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
 
-#endif /* SORT_H */
+#endif
