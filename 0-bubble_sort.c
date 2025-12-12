8023d3f0e052ea2013c1d13cd352ef8becfc061b
yorichill <thomas.haenel101@gmail.com>
2025-12-12 12:39:01 +0100

    sort.h correction

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,38 +1,30 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Trie un tableau d'entiers par ordre croissant (Bubble Sort)
- * @array: Le tableau à trier
- * @size: La taille du tableau
+ * bubble_sort - Sorts an array of integers in ascending order
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    int swapped;
+	size_t i, j;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                /* Échange des éléments */
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                
-                /* Affichage du tableau après l'échange */
-                print_array(array, size);
-                swapped = 1;
-            }
-        }
-        /* Si aucun échange n'a eu lieu, le tableau est déjà trié */
-        if (swapped == 0)
-            break;
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				/* Appel obligatoire après chaque swap */
+				print_array(array, size);
+			}
+		}
+	}
 }
