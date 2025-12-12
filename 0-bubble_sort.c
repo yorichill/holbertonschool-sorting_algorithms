461327e5b4e0bb1ae56044eeb432dc792c9ce809
yorichill <thomas.haenel101@gmail.com>
2025-12-12 12:50:14 +0100

    add

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,38 +1,41 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- * using the Bubble sort algorithm.
- *
- * @array: The array to be sorted.
- * @size: Number of elements in @array.
- */
+* bubble_sort - Sorts an array of integers in ascending order
+* using the Bubble sort algorithm.
+* @array: The array to be sorted.
+* @size: The size of the array.
+*
+* Description: Prints the array after each swap of two elements.
+*/
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+size_t i, j;
+int temp;
+int swapped;
 
-	/* Vérification de base : tableau vide ou un seul élément */
-	if (array == NULL || size < 2)
-		return;
+if (array == NULL || size < 2)
+return;
 
-	/* Boucle principale pour parcourir tout le tableau */
-	for (i = 0; i < size - 1; i++)
-	{
-		/* Boucle de comparaison des éléments adjacents */
-		for (j = 0; j < size - i - 1; j++)
-		{
-			/* Si l'élément actuel est plus grand que le suivant */
-			if (array[j] > array[j + 1])
-			{
-				/* Échange des deux éléments */
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+for (i = 0; i < size - 1; i++)
+{
+swapped = 0; /* Pour optimiser : on vérifie si on a fait un échange */
+for (j = 0; j < size - 1 - i; j++)
+{
+if (array[j] > array[j + 1])
+{
+/* Échange (Swap) */
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
 
-				/* Affichage OBLIGATOIRE après chaque échange */
-				print_array(array, size);
-			}
-		}
-	}
+/* Affichage requis après chaque échange */
+print_array(array, size);
+swapped = 1;
+}
+}
+/* Si aucun échange n'a été fait dans ce passage, c'est trié */
+if (swapped == 0)
+break;
+}
 }
--- sort.h
+++ sort.h
@@ -2,7 +2,6 @@
 #define SORT_H
 
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -17,11 +16,13 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-/* Prototypes fournis par Holberton pour l'affichage */
+/* Fonctions d'affichage (fournies) */
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Prototypes des algorithmes */
+ /* pour les task */
+void quick_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
