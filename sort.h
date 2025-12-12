a5ee8605a52ff9fffeb913b64f7e8bca92d60127
yorichill <thomas.haenel101@gmail.com>
2025-12-12 12:36:48 +0100

    sort.h correction

--- sort.h
+++ sort.h
@@ -1,10 +1,13 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
+ *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -16,15 +19,14 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-/* Fonctions d'affichage (fournies) */
-
+/* Helper functions (Fournies dans votre projet) */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
- /* pour les task */
-void quick_sort(int *array, size_t size);
+/* Prototypes des algorithmes de tri */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
